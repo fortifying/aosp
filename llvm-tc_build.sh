13079a8d1726cb33343be817d29106d43e096765
Heinz Der Flugel <heinzdf@gabuterscape.my.id>
2021-09-30 20:35:09 +0700

    Track repo

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Use xRageTC build script as LLVM Build Script.
-git clone https://github.com/xyz-prjkt/llvmTC $(pwd)/llvmTC -b llvm-tc
-cd $(pwd)/llvmTC
+git clone https://$GH_TOKEN@github.com/fortifying/llvm $(pwd)/llvm -b llvm-tc
+cd $(pwd)/llvm
 bash build-tc.sh
